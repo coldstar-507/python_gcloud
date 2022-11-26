a375de3047ab62f26a31fd4a891d05cdef4edbd7
coldstar-507 <scott.1212@gmx.com>
2022-11-26 00:33:46 -0500

    need to fix upload error code=3

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import functions_framework, replicate, requests, flask, io, base64
+import functions_framework, replicate, requests, flask, base64
 
 from replicate.exceptions import ModelError
 
-model = replicate.models.get('stability-ai/stable-diffusion')
+client = replicate.Client(api_token="3e352b505e7f33370501180343d5443169fe1cce")
+
+model = client.models.get('stability-ai/stable-diffusion')
+
 
 @functions_framework.http
 def imageGenerationRequest(request: flask.Request):
@@ -36,7 +39,7 @@ def imageGenerationRequest2(request: flask.Request):
     listOfPrompts = request.get_json()
     for prompt in listOfPrompts:
         try:
-            out = model.predict(prompt=prompt, width=512, height=512, num_outputs=1, num_inference_steps=12, guidance_scale=7.5)
+            out = model.predict(prompt=prompt, width=512, height=512, num_outputs=1, num_inference_steps=10, guidance_scale=7.5)
             response = requests.get(out[0])
             if response.ok:
                 return flask.jsonify(image=base64.b64encode(response.content).decode("utf-8"), prompt=prompt)
